1be4170473f36ce365d2284e0dc3e2112b3f826b
jizsam <46747330+jizsam@users.noreply.github.com>
2021-01-05 21:33:36 +0530

    first commit

---
+++ plantcode.js
@@ -0,0 +1,47 @@
+
+    class Plant{
+
+        static getAccountDetails=()=>{
+            var accountDetails={
+                userone:{email:"abc@gmail.com",password:"testuser1"},
+            
+                usertwo:{email:"jiz@gmail.com",password:"testuser2"}
+            
+            }
+
+            return accountDetails;
+
+        }
+
+
+        
+        static login=()=>{
+            let username=document.querySelector("#username").value;
+            let password=document.querySelector("#password").value;
+             let dataset= Plant.getAccountDetails();
+             if(username in dataset){
+                 if(password===dataset[username].password){
+                     alert("Sign in successful");
+                     window.location.href="index.html";
+                 }
+                 else{
+                     alert("Invalid password");
+                 }
+             }
+             else{
+                 alert("Invalid username");
+             }
+
+            }
+        }
+
+       
+
+        
+
+        
+
+
+
+    
+    
